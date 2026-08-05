46aaf2a24f87082d1fe2aeb488580d72b07a0d5f
neobiolinkoficial-code <neobiolinkoficial@gmail.com>
2026-08-05 15:33:53 -0300

    Delete layout.js

--- layout.js
+++
@@ -1,3 +0,0 @@
-
-export const metadata = { title: 'NEO BIO - Gerador Bio Sites', description: 'PWA 100% LOCAL' }
-export default function RootLayout({children}){ return <html lang="pt-BR"><body style={{margin:0,fontFamily:'Inter, sans-serif',background:'#0A0A0A',color:'white'}}>{children}</body></html> }
